b171a7d45e30b35bdb47f90fc7a7f11a79870350
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2018-12-29 09:17:25 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install vlc arduino codeblocks
+wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
+sudo apt-get install teamviewer-host_armhf.deb
+sudo teamviewer daemon enable
+sudo teamviewer passwd 12341234
+sudo apt remove squeak smartsim minecraft-pi sense_emu_gui
+rm ~/.config/lxpanel/LXDE-pi/panels/panel
+mv ~/Downloads/panel ~/.config/lxpanel/LXDE-pi/panels/panel
