783184927250d4f6a72b24f3480c620ba9af8727
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2019-03-27 13:20:38 +0300

    Update start.sh
    my files only!!!

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt install vlc arduino codeblocks
+sudo apt install vlc arduino ntfs-3g php ngnix qtcreator qt5-default
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
 sudo apt-get install teamviewer-host_armhf.deb
 sudo teamviewer daemon enable
@@ -8,3 +7,4 @@ sudo teamviewer passwd 12341234
 sudo apt remove squeak smartsim minecraft-pi sense_emu_gui
 rm ~/.config/lxpanel/LXDE-pi/panels/panel
 mv ~/Downloads/panel ~/.config/lxpanel/LXDE-pi/panels/panel
+sudo reboot
